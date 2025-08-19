0465b6f36339f9742a59aed23ba2da4a9c8e7eba
ashbit_ <ashbit0192@gmail.com>
2025-08-19 00:46:44 -0400

    settings are now saved

---
+++ src/Settings.java
@@ -0,0 +1,21 @@
+import java.util.prefs.Preferences;
+
+public class Settings {
+    private static final Preferences prefs = Preferences.userNodeForPackage(Settings.class);
+
+    public static void saveLastWorld(String world) {
+        prefs.put("lastWorld", world);
+    }
+
+    public static String getLastWorld() {
+        return prefs.get("lastWorld", null); // null if not set
+    }
+
+    public static void saveLastJourneymap(String path) {
+        prefs.put("lastJourneymap", path);
+    }
+
+    public static String getLastJourneymap() {
+        return prefs.get("lastJourneymap", null);
+    }
+}
